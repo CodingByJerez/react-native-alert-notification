46f7784b2e5d9ca68f8cb022138922ff95104ed2
Coding By Jerez <52014882+CodingByJerez@users.noreply.github.com>
2022-07-29 15:33:46 +0200

    fix: 🐛 remove file polifile

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { ALERT_TYPE } from './config/ENV';
 import { Dialog, IConfigDialog, IConfigToast, Root, Toast } from './containers';
-import './polyfills';
 
 export { Root, Dialog, Toast, ALERT_TYPE, IConfigDialog, IConfigToast };
 export default { Root, Dialog, Toast, ALERT_TYPE };
